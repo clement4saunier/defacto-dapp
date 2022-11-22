8cb2b9383f0ecedd7071622672f6d3fab09c04cf
Maxime NOEL <maxime.noel@epitech.eu>
2022-11-22 17:44:32 +0100

    [ADD] text home page presentation

--- frontend/src/components/page/Home.jsx
+++ frontend/src/components/page/Home.jsx
@@ -21,13 +21,25 @@ export default function Home() {
   return (
     <>
       <h1>Decentralized fact-checking and journalism platform</h1>
-      {text}
+      <p>Defacto is a decentralize platform where you can ask if a news is real or fake.</p>
       <button onClick={() => navigate('/list')}>List <Icon crypto="send-in"/></button>
       <button onClick={() => navigate('/create')}>Create <Icon crypto="send-in"/></button>
       <h2>What is it about ?</h2>
-      {Array(3).fill(text)}
+      {text}
       <h2>How does it work ?</h2>
-      {Array(6).fill(text)}
+      <p> We have 3 differents roles on the platform.</p>
+      <ul>
+        <li>The <strong>Asker</strong> will post a request with details of it, the information to be verify, the bounty and the timer 
+        for the fact-checker, and the number of delegates. Anybody can be a Asker </li>
+        <li>The <strong>Fact-Checker</strong>. His role represent the most of the work, he will investigate to found if yes or not the 
+        information is real/fake. Let's suppose that a request was posted, the fact-checker have the possibility to choose if he wants 
+        to verify it and he will communicate his report with the most sources as possible before the end of the timer. This time again 
+        anyone can be a Fact-checker.</li>
+        <li>The <strong>Deleguate</strong>. If the fact-checker will provide most of the works it's the delegate who has the most 
+        tricky part, he will validates or not if the fact-checker is right or if it is a bad report. When the deleguate prononces the 
+        result the bounty is automatically share between the fact-checker and the delegaute.
+        </li>
+      </ul>
     </>
   );
 }
