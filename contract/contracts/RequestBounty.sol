024b732cdd0a72c1802276fe4e951f0bc199f534
clement <clement.saunier@epitech.eu>
2022-11-22 23:11:47 +0100

    [Add] Multiple blockchain and ipfs source provider

--- contract/contracts/RequestBounty.sol
+++ contract/contracts/RequestBounty.sol
@@ -4,10 +4,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RequestBounty {
     struct Request {
-        address sender;
+        address owner;
         string content;
         IERC20 token;
         uint256 amount;
+        uint deadline;
     }
 
     struct Response {
@@ -24,15 +25,50 @@ contract RequestBounty {
     //Mapping from requestId to responseId to Response
     mapping(uint256 => mapping(uint256 => Response)) response;
 
-    function publishRequest(string memory content, IERC20 bountyToken, uint256 bountyAmount) external returns (uint256 requestId) {
-        requestId = uint256(keccak256(abi.encodePacked(content)));
-        
-        // /!\ need require() to check if request doesn't already exist
-        request[requestId] = Request(msg.sender, content, bountyToken, bountyAmount);
-        emit Publish(requestId);
+    function _mintRequest(
+        uint256 id,
+        address to,
+        string memory content,
+        IERC20 bountyToken,
+        uint256 bountyAmount,
+        uint deadline
+    ) internal {
+        require(
+            bountyToken.allowance(to, address(this)) >= bountyAmount &&
+                bountyToken.transferFrom(to, address(this), bountyAmount),
+            "Token deposit failed."
+        );
+        request[id] = Request(to, content, bountyToken, bountyAmount, deadline);
+        emit Publish(id);
     }
 
-    function publishResponse(uint256 requestId, string memory content) external returns (uint256 responseId) {
+    function publishRequest(
+        string memory content,
+        IERC20 bountyToken,
+        uint256 bountyAmount,
+        uint deadline
+    ) external returns (uint256 requestId) {
+        requestId = uint256(keccak256(abi.encodePacked(content)));
+        require (request[requestId].owner == address(0), "Request already posted");
+
+        _mintRequest(
+            requestId,
+            msg.sender,
+            content,
+            bountyToken,
+            bountyAmount,
+            deadline
+        );
+    }
+
+    function requestURI(uint256 requestId) public view returns (string memory) {
+        return request[requestId].content; //ipfs://QMMLI3120SDFMKLSDJFJE
+    }
+
+    function publishResponse(uint256 requestId, string memory content)
+        external
+        returns (uint256 responseId)
+    {
         emit Respond(requestId, 0);
     }
 }
