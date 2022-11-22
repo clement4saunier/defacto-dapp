8b290d4cbf2ab381f820027e4ddaf7dee613524d
Mickael Riess <mickaelriess@mickaels-air.localdomain>
2022-11-22 22:24:43 +0100

    delegate NFT mintable

--- contract/contracts/DelegateRegistry.sol
+++ contract/contracts/DelegateRegistry.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DelegateRegistry is ERC721 {
-    constructor() ERC721("Delegate", "DLGT") {}
+contract DelegateRegistry is ERC721, Ownable {
+    constructor() ERC721("DelegateRegistry", "DLGT") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
 }
