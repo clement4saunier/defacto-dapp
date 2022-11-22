737aed7e34f31ddda4261d24e6b99a1cc55b38f1
clement <clement.saunier@epitech.eu>
2022-11-22 17:10:29 +0100

    [Add] Header navigation buttons

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,7 +1,9 @@
 import Icon from "../content/Icon";
 import { useThemeContext } from "../context/ThemeProvider";
+import {useNavigate} from 'react-router-dom';
 
 export default function Header() {
+  let navigate = useNavigate();
   const { theme, themes, setAndStoreTheme } = useThemeContext();
 
   function onThemeSet(v) {
@@ -12,6 +14,11 @@ export default function Header() {
     <header>
       <div>
         <h3 style={{ margin: 0 }}><Icon crypto="list"/> Defacto</h3>
+      </div>
+      <div>
+        <button onClick={() => navigate("/")}>Home <Icon crypto="info"/></button>
+        <button onClick={() => navigate("/list")}>List <Icon crypto="search"/></button>
+        <button onClick={() => navigate("/create")}>Create <Icon crypto="send-in"/></button>
       </div>
       <div>
         <select value={theme.name} name="theme" onChange={onThemeSet}>
