0bb8bbd1cb97e88ca951177affd23b897e0d2acf
clement <clement.saunier@epitech.eu>
2022-11-23 17:37:23 +0100

    [Add] Infura IPFS uploader

--- frontend/src/components/page/create/Mint.jsx
+++ frontend/src/components/page/create/Mint.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useMemo, useState } from "react";
+import Icon from "../../content/Icon";
 import { useWeb3Context } from "../../context/Web3Provider";
 import useRequestBountyContract from "../../hooks/useRequestBountyContract";
 import { useCreationContext } from "../Create";
+import { create } from "ipfs-http-client";
+import {Buffer} from 'buffer';
 
 export default function Mint() {
   const {
@@ -23,22 +26,72 @@ export default function Mint() {
   } = useCreationContext();
   const { account } = useWeb3Context();
   const { instance } = useRequestBountyContract();
-  const [cid, setCid] = useState();
+  const [cid, setCid] = useState(null);
   const file = useMemo(
     () => JSON.stringify({ name: title, description: body }),
     [title, body]
   );
 
-  useEffect(() => {
-    //fetch IPFS CID
-    file && console.log("UPLOAD", file);
-  }, [file]);
+  const [selectedGatewayIndex, setSelectedGatewayIndex] = useState(0);
+  const [ipfsUploadGateways] = useState([
+    {
+      name: "Starton API",
+      upload: async (cid) =>
+        "1"
+    },
+    {
+      name: "Infura",
+      upload: async (file) => {
+
+        console.log("GEE");
+        // /!\ Trash api secrets, these won't go beyond free package
+        const projectId = "2DKn2VvZwVq1UvT9fOSUIOatVXZ";
+        const infura = "a870dea9a4a3c50b9dc7021b671fbaed";
+
+        const client = create({
+          host: "ipfs.infura.io",
+          port: 5001,
+          protocol: "https",
+          headers: {
+            authorization:
+              "Basic " +
+              Buffer.from(projectId + ":" + infura).toString("base64")
+          }
+        });
+
+        if (!file) return undefined;
+
+        try {
+          const cid = await client.add(file);
+          return cid.path;
+        } catch (error) {
+          console.log("Error uploading file: ", error);
+          return undefined;
+        }
+      }
+    }
+  ]);
+  const gateway = useMemo(
+    () => ipfsUploadGateways[selectedGatewayIndex],
+    [selectedGatewayIndex]
+  );
 
   function onMintButton() {
     const args = [cid, token, "0", 1903123123];
     console.log(`publishRequest(${[...args]})`);
     instance.publishRequest(...args);
   }
+
+  function onUploadButton() {
+    async function fetchCid() {
+        setCid(undefined);
+      setCid(await gateway.upload(file));
+    }
+
+    file && fetchCid();
+  }
+
+  useEffect(() => console.log("cid", cid), [cid]);
 
   return (
     <>
@@ -48,6 +101,24 @@ export default function Mint() {
       <h3>{title}</h3>
       <p>{body}</p>
       <div className="divider" />
+      <h3>IPFS Upload gateway</h3>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          {ipfsUploadGateways.map(({ name }, index) => (
+            <button
+              className={index !== selectedGatewayIndex && "unselected"}
+              key={index}
+              onClick={() => setSelectedGatewayIndex(index)}
+            >
+              {name}
+            </button>
+          ))}
+        </div>
+        <button onClick={onUploadButton}>
+          {cid === null ? "Upload" : cid === undefined ? "Loading..." : cid}{" "}
+          <Icon crypto="send-in" />
+        </button>
+      </div>
       <p>
         Will be subimitted by <a>{account.substring(0, 7)}</a> for {bounty} of{" "}
         {token}
