1717dde4e6f5de8da2209190fcf01936ebd03eb5
Alexandre Schaffner <alexandre.schaffner@icloud.com>
2022-11-23 04:23:29 +0100

    [STYLE]

--- backend/src/controllers/getStatementCtrl.ts
+++ backend/src/controllers/getStatementCtrl.ts
@@ -4,6 +4,7 @@ import { getStatement } from '../services/statements'
 export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const statement = await getStatement(req.params.id)
+
     res.send({ success: true, statement })
   } catch (err: unknown) {
     next(err)
