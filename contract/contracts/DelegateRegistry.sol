5271587ae36548313f11f9b7d2c1621687d6626d
Mickael Riess <mickaelriess@mickaels-air.localdomain>
2022-11-24 03:55:31 +0100

    set URI of tokens

--- contract/contracts/DelegateRegistry.sol
+++ contract/contracts/DelegateRegistry.sol
@@ -5,25 +5,42 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DelegateRegistry is ERC721, Ownable {
 
+
     uint256 public minimumDonationValue = 0.1 ether;
     uint256 public currentSupply;
     uint256 public totalDonationsReceived;
     mapping(address =>  uint256) public delegateWallets;
 
+    mapping(uint256 => string) private _tokenURIs;
+
     constructor() ERC721("DelegateRegistry", "DLGT") {
         currentSupply = 0;
     }
 
-    function increaseCurrentSupply() private {
+    function increaseCurrentSupply() internal {
         currentSupply += 1;
     }
 
-    function mint(address to) external onlyOwner {
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+    
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        return _tokenURI;
+    }
+    
+
+    function mint(address to, string memory tokenURI_) external onlyOwner {
         require(delegateWallets[to] < 1, 'Only one delegate badge per wallet');
         delegateWallets[to]++;
         increaseCurrentSupply();
-        uint256 tokenId = currentSupply;
-        _safeMint(to, tokenId);
+        uint256 newItemId = currentSupply;
+        _mint(to, newItemId);
+        _setTokenURI(newItemId, tokenURI_);
     }
 
     function donate() external payable {
