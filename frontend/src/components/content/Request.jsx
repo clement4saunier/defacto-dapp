72d9de57c5efc332a5623bc0f2f46a0ae89c4f28
clement <clement.saunier@epitech.eu>
2022-11-24 10:24:41 +0100

    [Add] Transaction has link for requests

--- frontend/src/components/content/Request.jsx
+++ frontend/src/components/content/Request.jsx
@@ -5,19 +5,22 @@ import { useEffect } from "react";
 import Icon from "./Icon";
 import { useMemo } from "react";
 import Currency from "./Currency";
+import { explorers } from "../../contracts/explorers";
+import { useWeb3Context } from "../context/Web3Provider";
 
 export default function Request({
   address,
-  chainId,
   id,
   cid,
   fetchCid,
   origin,
+  hash,
   owner,
   amount,
   symbol
 }) {
   let navigate = useNavigate();
+  const { chainId } = useWeb3Context();
   const [metadata, setMetadata] = useState({});
   const [showDetails, setShowDetails] = useState(false);
   const originDate = useMemo(() => {
@@ -46,42 +49,34 @@ export default function Request({
     cid && fetchRequestMetadata();
   }, [cid, fetchCid]);
 
-  // useEffect(() => {
-  //   setCid(requestCid);
-  // }, [address, chainId, requestId, requestCid]);
-
   return (
-    <div
-      className={[styles.card, "card"].join(" ")}
-      onClick={(e) => {
-        navigate("/request/" + id);
-        e.stopPropagation();
-      }}
-    >
-      <div>
+    <div className={[styles.card, "card"].join(" ")}>
+      <div
+        className={styles.innerCard}
+        onClick={(e) => {
+          navigate("/request/" + id);
+          e.stopPropagation();
+        }}
+      >
         <h3>
-          {metadata ? (
-            metadata.name
-          ) : metadata === null ? (
+          {metadata && metadata.name}
+          {!metadata && metadata === null && (
             <span className="error">
               <Icon crypto="denied" /> Couldn't fetch content
             </span>
-          ) : (
-            "Loading..."
           )}
+          {metadata === undefined && "Loading..."}
         </h3>
         <p>
           Submitted by <a>{(owner && owner.substring(0, 7)) ?? "Loading..."}</a>{" "}
           for <a>{(amount && amount.toNumber()) ?? "..."} </a>
           <Currency symbol={symbol} />
         </p>
+        <div className={styles.status}>
+          <span className={["highlight", "success"].join(" ")}>active</span>
+        </div>
       </div>
       <div className={[styles.details, showDetails && "show"].join(" ")}>
-        <p>
-          <span className="highlight success">active</span>
-          <br />
-          <br />
-        </p>
         <div
           onClick={(e) => {
             setShowDetails((show) => !show);
@@ -94,8 +89,29 @@ export default function Request({
         </div>
         <div className={[styles.chain, showDetails && styles.show].join(" ")}>
           <p>
-            @<a>{address.substring(0, 8)}</a>
+            @
+            <a
+              href={
+                explorers.get(chainId)
+                  ? explorers.get(chainId).address(address)
+                  : address
+              }
+              target="_blank"
+            >
+              {address.substring(0, 8)}
+            </a>
             <br />#<a>{id.substring(0, 8)}</a>
+            <br />h
+            <a
+              href={
+                explorers.get(chainId)
+                  ? explorers.get(chainId).txn(hash)
+                  : address
+              }
+              target="_blank"
+            >
+              {hash && hash.substring(0, 8)}
+            </a>
             <br />t<a>{originDate}</a>
           </p>
         </div>
