92575f248be633a0a62a7b6470824a00e2010d1a
clement <clement.saunier@epitech.eu>
2022-11-26 23:13:36 +0100

    [Add] Testnet faucet

--- contract/contracts/DefactoToken.sol
+++ contract/contracts/DefactoToken.sol
@@ -6,4 +6,12 @@ contract DefactoToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 100000000000);
     }
+
+    function mint(uint256 amount) external {
+        _mint(msg.sender, amount);
+    }
+
+    function decimals() override {
+        return 9;
+    }
 }
