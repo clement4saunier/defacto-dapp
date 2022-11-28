caeea468f95f94ae7339ee903361d3cd549d7a7e
Alexandre Schaffner <alexandre.schaffner@icloud.com>
2022-11-28 05:27:04 +0100

    [UPDATE] bounty form for requester

--- frontend/src/components/page/create/Fill.jsx
+++ frontend/src/components/page/create/Fill.jsx
@@ -66,186 +66,67 @@ export default function Fill() {
           id="textAreaBody"
         />
 
-
-        <div className="test">
-        <div className="parent">
-          <h3>Bounty</h3>
-
-          <div className="bounty" style={{}}>
+        <div className="parent" style={{display: "flex", marginTop: 'var(--space-xl)', marginBottom: '30px'}}>
+        <h3>Bounty</h3>
+          <div className="bounty" style={{display: "flex"}}>
+            <div className="child-1" style={{width: 75 + "%"}}>
               <input
                 name="bounty"
                 value={bounty}
                 required
-                style={{fontSize: 42 + 'px'}}
+                style={{fontSize: 28 + 'px', width: 100 + "%"}}
                 onChange={(e) => setBounty(e.target.value)}>
               </input>
+            </div>
+            <div className="child-2"style={{width: 25 + "%"}}>
               <select
                className="token"
                 value={symbol}
                 onChange={(e) => setSymbol(e.target.value)}
                 required
-                style={{fontSize: 42 + 'px'}}
+                style={{fontSize: 28 + 'px', width: 100 + "%"}}
                 >
                 {currencies.chain[chainId] &&
                   currencies.chain[chainId].map(({ name, address }, idx) => (
                     <option key={idx}>{name}</option>
                   ))}
               </select>
+            </div>
           </div>
 
-          <div className="delegate">
-            <p /*style={{fontSize: 15 + 'px'}}*/>Enter the name of your delegate of choice</p>
-              <div>
+            <p>Choose a delegate</p>
+          <div className="delegate" style={{display: "flex"}}>
+              <div style={{width: "100%"}}>
                 <select
                 name="delegate"
                 value={delegate}
-                style={{fontSize: 42 + 'px', minWidth: 100 + '%', width: 0}}
+                style={{fontSize: 28 + 'px', width: 100 + "%"}}
                 onChange={(e) => setDelegate(e.target.value)}
                 placeholder="Enter the name of your delegate of choice"
                 >
                 <option value="defacto">DeFacto</option>
               </select>
               </div>
-              <div>
-              <select
-                value={timer}
-                onChange={(e) => setTimer(e.target.value)}
-                required
-                style={{fontSize: 42 + 'px', minWidth: 100 + '%', whiteSpace: 'nowrap'}}
-                >
-                <option value="24h">24H</option>
-                <option value="48H">48H</option>
-                <option value="72H">72H</option>
-                <option value="one week">1 week</option>
-                <option value="one month">1 month</option>
-              </select>
+          </div>
+          <p>Choose when your request will expire</p>
+              <div style={{width: 100 + "%"}}>
+                <select
+                  value={timer}
+                  onChange={(e) => setTimer(e.target.value)}
+                  required
+                  style={{fontSize: 28 + 'px', whiteSpace: 'nowrap', width: 100 + "%"}}
+                  >
+                  <option value="24h">24H</option>
+                  <option value="48H">48H</option>
+                  <option value="72H">72H</option>
+                  <option value="one week">1 week</option>
+                  <option value="one month">1 month</option>
+                </select>
               </div>
-          <button title="Submit" type="submit" style={{width: 100 + '%', fontSize: 42 + 'px', marginTop: 'var(--space-xl)', color: 'var(--color-background)'}}>
-            Add to verify
-          </button>
           </div>
-          </div>
-      </div>
-
-        {/* <div className="questionForm">
-          <p>Enter the currency and amount you offer as bounty for your request</p>
-          <span>
-            <input
-              name="bounty"
-              value={bounty}
-              required
-              onChange={(e) => setBounty(e.target.value)}
-            />{" "}
-            {token && token.substring(0, 7)}{" "}
-            <select
-              value={symbol}
-              onChange={(e) => setSymbol(e.target.value)}
-              required
-            >
-              {currencies.chain[chainId] &&
-                currencies.chain[chainId].map(({ name, address }, idx) => (
-                  <option key={idx}>{name}</option>
-                ))}
-            </select>
-          </span>
-        </div>
-        <div className="questionForm">
-          <p>
-            Enter the duration of your bounty.
-          </p>
-          <select
-            value={timer}
-            onChange={(e) => setTimer(e.target.value)}
-            required
-            style={{fontSize: 40 + 'px'}}
-          >
-            <option value="24h">24H</option>
-            <option value="48H">48H</option>
-            <option value="72H">72H</option>
-            <option value="one week">1 week</option>
-            <option value="one month">1 month</option>
-          </select>
-        </div>
-        <div className="questionForm">
-          <p>Enter the name of your delegate of choice</p>
-            <input
-              name="delegate"
-              value={delegate}
-              onChange={(e) => setDelegate(e.target.value)}
-            />
-        </div>
-        <div style={{ display: "flex", justifyContent: "right" }}>
-          <button title="Submit" type="submit">
-            Add to verify
+          <button title="Submit" type="submit" style={{width: 100 + '%', fontSize: 36 + 'px', marginTop: 'var(--space-s)', marginBottom: '40px', color: 'var(--color-background)'}}>
+            Post your request
           </button>
-        </div>
-        <br /> */}
-
-
-
-
-
-
-
-        {/*
-        <div className="questionForm">
-          <p>Enter the currency and amount you offer as bounty for your request</p>
-          <span>
-            <input
-              name="bounty"
-              value={bounty}
-              required
-              onChange={(e) => setBounty(e.target.value)}
-            />{" "}
-            {token && token.substring(0, 7)}{" "}
-            <select
-              value={symbol}
-              onChange={(e) => setSymbol(e.target.value)}
-              required
-            >
-              {currencies.chain[chainId] &&
-                currencies.chain[chainId].map(({ name, address }, idx) => (
-                  <option key={idx}>{name}</option>
-                ))}
-            </select>
-          </span>
-        </div>
-        <div className="questionForm">
-          <p>
-            Enter the duration of your bounty.
-          </p>
-          <select
-            value={timer}
-            onChange={(e) => setTimer(e.target.value)}
-            required
-            style={{fontSize: 40 + 'px'}}
-          >
-            <option value="24h">24H</option>
-            <option value="48H">48H</option>
-            <option value="72H">72H</option>
-            <option value="one week">1 week</option>
-            <option value="one month">1 month</option>
-          </select>
-        </div>
-        <div className="questionForm">
-          <p>Enter the name of your delegate of choice</p>
-            <select
-              name="delegate"
-              value={delegate}
-              onChange={(e) => setDelegate(e.target.value)}
-            >
-              <option value='defacto'>DeFacto</option>
-              <option value='afp'>AFP</option>
-              <option value='amnesty'>Amnesty International</option>
-              <option value='politifacts'>Politifacts</option>
-            </select>
-        </div>
-        <div style={{ display: "flex", justifyContent: "right" }}>
-          <button title="Submit" type="submit">
-            Add to verify
-          </button>
-        </div>
-        <br /> */}
       </form>
     </>
   );
